09298f0858f018ac8875b2744ba8c5dc4bcfd71d
Pavel Kalinovskii <p.kalinovskii@gmail.com>
2020-04-07 09:15:42 +0300

    io tasks and conditions tasks

---
+++ conditions.py
@@ -0,0 +1,17 @@
+"""
+#sign
+x = int(input())
+if x > 0:
+    sign = 1
+elif x < 0:
+    sign = -1
+elif x == 0:
+    sign = 0
+print(sign)
+
+#min
+a, b = int(input())
+if a < b: 
+    print(a)
+print(b)
+"""
---
+++ input_output.py
@@ -0,0 +1,22 @@
+"""
+# watch
+n = int(input())
+print((n % 1440) // 60, n % 60)
+
+#Hello
+name = input()
+print('Hello, ' + name + '!')
+
+n = int(input())
+next_n = n + 1
+prev_n = n - 1
+print('The next number for the number ' + str(n) + ' is ' + str(next_n))
+print('The previous number for the number ' + str(n) + ' is ' + str(prev_n))
+
+a = int(input())
+b = int(input())
+l = int(input())
+n = int(input())
+
+print((b * (n - 1) * 2) + (a * (n - 1) * 2) + (l * 2) + a)
+"""
